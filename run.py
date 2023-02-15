784dba9d88ecd6559a06fec5272b1858f7988d5e
Daithí <davecoll3@gmail.com>
2023-02-15 17:34:06 +0000

    tables added to taskmanager database

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
+from taskmanager import db
 from taskmanager import app
+
 
 
 if __name__ == "__main__":
@@ -8,3 +10,13 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
+
+
+def create_tables(self):
+    with app.app_context():
+        db.create_all()
+
+
+create_tables(db)
+
+
